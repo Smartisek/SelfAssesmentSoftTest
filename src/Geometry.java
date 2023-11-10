b0a4ab191f60f49a18ec26c706e9897995be7e9f
Smartisek <domalipdominik@gmail.com>
2023-11-10 11:59:19 +0000

    Creating test

--- src/Geometry.java
+++ src/Geometry.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Geometry {
+public class Geometry {
+
+    public String classify(int sideA, int sideB, int sideC){
+
+        boolean triangle = (((sideA+sideB) > sideC) && ((sideB + sideC) > sideA) && ((sideA+sideC) > sideB));
+        if(!triangle)
+            return "Not a triangle";
+        else {
+            int match = 0;
+            if(sideA == sideB)
+                match = match +1;
+            if(sideB == sideC)
+                match = match+1;
+            if(sideA == sideC)
+                match = match +1;
+            if(match == 0)
+                return "Scalane";
+            if(match ==1)
+                return "Isosceles";
+            else
+                return "Equilateral";
+        }
+    }
 }
--- src/GeometryTest.java
+++ src/GeometryTest.java
@@ -1,4 +1,62 @@
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+
 class GeometryTest {
-  
+    @Test
+    void IsntTriangle(){
+        int a = 10;
+        int b = 4;
+        int c = 15;
+
+        Geometry g = new Geometry();
+        String expected = "Not a triangle";
+        String actual = g.classify(a,b,c);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+    void IsScalane(){
+        int a = 20;
+        int b = 10;
+        int c = 15;
+
+        Geometry g = new Geometry();
+        String expected = "Scalane";
+        String actual = g.classify(a,b,c);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+    void IsIsosceles(){
+        int a = 20;
+        int b = 15;
+        int c = 15;
+
+        Geometry g = new Geometry();
+        String expected = "Isosceles";
+        String actual = g.classify(a,b,c);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+    void IsEquilateral(){
+        int a = 15;
+        int b = 15;
+        int c = 15;
+
+        Geometry g = new Geometry();
+        String expected = "Equilateral";
+        String actual = g.classify(a,c,b);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
 }
