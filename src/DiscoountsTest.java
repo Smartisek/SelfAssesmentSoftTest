ab52bbb0b5df184b96b24a8769be08a03d13f0d9
Smartisek <domalipdominik@gmail.com>
2023-11-10 16:27:37 +0000

    rest of the tests

--- src/DiscoountsTest.java
+++ src/DiscoountsTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DiscoountsTest {
 
     @Test
-    void checkDiscount() {
+    void Discount9() {
         boolean days10 = true;
         boolean special = false;
         int amount = 1100;
@@ -16,5 +16,81 @@ class DiscoountsTest {
         double actual = d.CheckDiscount(days10, special, web, amount);
 
         assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+    void NoDiscountDays(){
+        boolean days10 = false;
+        boolean special = true;
+        int amount = 759;
+        boolean web = true;
+
+        Discoounts d = new Discoounts();
+        double expected = 0;
+        double actual = d.CheckDiscount(days10,special,web,amount);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+    void Discount2(){
+        boolean days10 = true;
+        boolean special = false;
+        int amount = 759;
+        boolean web = false;
+
+        Discoounts d = new Discoounts();
+        double expected = 0.02;
+        double actual = d.CheckDiscount(days10,special,web,amount);
+
+        assertEquals(expected,actual);
+        System.out.println(actual);
+    }
+
+    @Test
+    void Discount7(){
+        boolean days10 = true;
+        boolean special = false;
+        int amount = 759;
+        boolean web = true;
+
+        Discoounts d = new Discoounts();
+        double expected = 0.07;
+        double actual = d.CheckDiscount(days10,special,web,amount);
+
+        assertEquals(expected,actual);
+        System.out.println(actual);
+    }
+
+    @Test
+    void Discount4(){
+        boolean days10 = true;
+        boolean special = false;
+        int amount = 1200;
+        boolean web = false;
+
+        Discoounts d = new Discoounts();
+        double expected = 0.04;
+        double actual = d.CheckDiscount(days10,special,web,amount);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @Test
+    void NoDiscountSpecial(){
+        boolean days10 = true;
+        boolean special = true;
+        int amount = 759;
+        boolean web = true;
+
+        Discoounts d = new Discoounts();
+        double expected = 0;
+        double actual = d.CheckDiscount(days10,special,web,amount);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
     }
 }
