b4fa625e07228a3d1ba8a3d47e7a8f83318d0f82
Smartisek <domalipdominik@gmail.com>
2023-11-10 16:43:28 +0000

    100% coverage for discount

--- src/DiscoountsTest.java
+++ src/DiscoountsTest.java
@@ -93,4 +93,19 @@ class DiscoountsTest {
         assertEquals(expected, actual);
         System.out.println(actual);
     }
+
+    @Test
+    void AmountLow(){
+        boolean days10 = true;
+        boolean special = false;
+        int amount = 259;
+        boolean web = true;
+
+        Discoounts d = new Discoounts();
+        double expected = 0;
+        double actual = d.CheckDiscount(days10,special,web,amount);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
 }
